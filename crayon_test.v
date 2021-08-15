f159ec7fa66250c98e10b9877ae730b71e4197e1
Vedant K <gamemaker0042@gmail.com>
2021-08-15 19:18:28 +0530

    fix: format files, update syntax

--- crayon_test.v
+++ crayon_test.v
@@ -1,16 +1,16 @@
 module crayon
 
 fn test_crayon() {
-	result := new("Hello world").bold().str()
-	assert result == "\e[1mHello world\e[0m"
+	result := new('Hello world').bold().str()
+	assert result == '\e[1mHello world\e[0m'
 }
 
-fn test_template(){
-	assert color("{bold Hello world}") == "\e[1mHello world\e[0m"
-	assert color("{bold.cyan Hello world}") == "\e[36;1mHello world\e[0m"
-	assert color("{bold.cyan.reverse Hello world}") == "\e[36;1;7mHello world\e[0m"
-	assert color("{bold Hello world} {red.reverse Hello again}") == "\e[1mHello world\e[0m \e[31;7mHello again\e[0m"
-	assert color("{}") == "{}"
-	assert color("{bold {{{Hello world}}}}") == "\e[1m{{{Hello world}}}\e[0m"
-	assert color("{rgb(10,10,10) Hello world}") == "\e[38;2;10;10;10mHello world\e[0m"
+fn test_template() {
+	assert color('{bold Hello world}') == '\e[1mHello world\e[0m'
+	assert color('{bold.cyan Hello world}') == '\e[36;1mHello world\e[0m'
+	assert color('{bold.cyan.reverse Hello world}') == '\e[36;1;7mHello world\e[0m'
+	assert color('{bold Hello world} {red.reverse Hello again}') == '\e[1mHello world\e[0m \e[31;7mHello again\e[0m'
+	assert color('{}') == '{}'
+	assert color('{bold {{{Hello world}}}}') == '\e[1m{{{Hello world}}}\e[0m'
+	assert color('{rgb(10,10,10) Hello world}') == '\e[38;2;10;10;10mHello world\e[0m'
 }
--- styles.v
+++ styles.v
@@ -1,37 +1,37 @@
 module crayon
 
 pub fn (c Crayon) bold() Crayon {
-	return c.stylize("bold")
+	return c.stylize('bold')
 }
 
 pub fn (c Crayon) dim() Crayon {
-	return c.stylize("dim")
+	return c.stylize('dim')
 }
 
 pub fn (c Crayon) italic() Crayon {
-	return c.stylize("italic")
+	return c.stylize('italic')
 }
 
 pub fn (c Crayon) underline() Crayon {
-	return c.stylize("underline")
+	return c.stylize('underline')
 }
 
 pub fn (c Crayon) slow_blink() Crayon {
-	return c.stylize("slow_blink")
+	return c.stylize('slow_blink')
 }
 
 pub fn (c Crayon) rapid_blink() Crayon {
-	return c.stylize("rapid_blink")
+	return c.stylize('rapid_blink')
 }
 
 pub fn (c Crayon) reverse() Crayon {
-	return c.stylize("reverse")
+	return c.stylize('reverse')
 }
 
 pub fn (c Crayon) hidden() Crayon {
-	return c.stylize("hidden")
+	return c.stylize('hidden')
 }
 
 pub fn (c Crayon) strikethrough() Crayon {
-	return c.stylize("strikethrough")
+	return c.stylize('strikethrough')
 }
